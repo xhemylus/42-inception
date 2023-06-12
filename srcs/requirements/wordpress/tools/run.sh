f0acee62d48394a85be9c342b0c58ab867877017
Lézarovitch <lezarovitch@protonmail.com>
2023-06-12 09:40:15 +0200

    Done on MacBook

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,6 +1,7 @@
 sleep 5
 
 wp core download --allow-root --path=/var/www/html/wordpress
-wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path=/var/www/html/wordpress --skip-check
+wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --path=/var/www/html/wordpress --skip-check
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=abollen@student.42lyon.fr --allow-root --path=/var/www/html/wordpress
+wp theme install --allow-root --activate --path=/var/www/html/wordpress zeever
 php-fpm7.3 -F
