98ec7490732ed1325043c6f0ecd9f283237d3bc0
Lézarovitch <lezarovitch@protonmail.com>
2023-06-15 09:44:48 +0200

    Marche po je crois mdr

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,12 +1,9 @@
 service mariadb start;
-echo "Bite";
-echo BITE - $SQL_ROOT_PASSWORD $SQL_DATABASE $SQL_USER $SQL_PASSWORD
-sleep 5;
-
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 mysql -u root -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');"
+
 mysqladmin -uroot -p${SQL_ROOT_PASSWORD} shutdown
