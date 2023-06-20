37a6127ce10b79f96a96729c0e4f57ea028bd57a
Lezarovitch <lezarovitch@protonmail.com>
2023-06-20 11:56:40 +0200

    Push

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,4 +1,4 @@
-wp core download --allow-root
+wp core download --allow-root --path=/var/www/html/wordpress
 wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --dbprefix=wp_ --skip-check --dbcollate="utf8_general_ci"
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=abollen@student.42lyon.fr --allow-root  --skip-email
 wp theme install --allow-root --activate --path=/var/www/html/wordpress zeever
