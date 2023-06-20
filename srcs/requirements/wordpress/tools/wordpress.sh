3ec231d4daa79d38a1aa22ba89855d2a61ce3112
Lezarovitch <lezarovitch@protonmail.com>
2023-06-20 12:33:04 +0200

    Push

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,13 +6,15 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /bin/wp
 	cd /var/www/html/wordpress/
-	wp core download --allow-root --version=$WORDPRESS_VERSION
-	wp config create --allow-root --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --dbcharset=$WORDPRESS_CHARSET --dbcollate="utf8_general_ci"
-	wp core install --allow-root --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email
-	wp user create --allow-root $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=author
+	wp core download --allow-root --version="6.2.2"
+	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --dbprefix=wp_ --dbcharset=utf8 --dbcollate="utf8_general_ci"
+	wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=abollen@student.42lyon.fr --skip-email
+	wp user create --allow-root "Bite" ¨bite@bite.com --user_pass=$WP_ADMIN_PASSWORD --role=author
 	wp theme install --allow-root --activate blocksy
 	chown -R www-data: /var/www/*
 	chmod -R 755 /var/www/html
+	env
+	sleep 10
 else
 	echo "Wordpress is already installed"
 fi
