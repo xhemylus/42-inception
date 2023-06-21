3fe7a5bc3e0b159198d25062cab73e07e64b4e3c
Lezarovitch <lezarovitch@protonmail.com>
2023-06-21 10:43:16 +0200

    Push

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,7 +1,7 @@
-wp core download --allow-root --path=/var/www/html/wordpress --version="6.2.2"
-wp config create --allow-root --path=/var/www/html/wordpress --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --dbprefix=wp_ --skip-check --dbcollate="utf8_general_ci"
-wp core install --url=abollen.42.fr --path=/var/www/html/wordpress --title=Titre --admin_user=root  --admin_password=1234 --admin_email=abollen@student.42lyon.fr --allow-root  --skip-email
-wp theme install --allow-root --activate --path=/var/www/html/wordpress zeever
+wp core download --allow-root --path=$WP_PATH --version=$WP_VERSION
+wp config create --allow-root --path=$WP_PATH --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$SQL_HOST --dbprefix=wp_ --skip-check --dbcollate="utf8_general_ci"
+wp core install --url=abollen.42.fr --path=$WP_PATH --title=Titre --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_EMAIL --allow-root  --skip-email
+wp theme install --allow-root --activate --path=$WP_PATH zeever
 chown -R www-data: /var/www/*
 chmod -R 755 /var/www/html
 exec "$@"
